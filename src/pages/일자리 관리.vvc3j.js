4acc3ce3746bcff5c96f88806b60df5ef8245369
rvjst <90255680+rvjst@users.noreply.github.com>
2024-06-19 01:14:32 +0900

    일자리 생성 연결

--- src/pages/일자리 관리.vvc3j.js
+++ src/pages/일자리 관리.vvc3j.js
@@ -7,42 +7,28 @@ $w.onReady(async function () {
     // Write your JavaScript here
     // To select an element by ID use: $w('#elementID')
     // Click 'Preview' to run your code
-    const jobUrl = "https://asdfdsas.p-e.kr/api/project/list"
-   
-    const jobResponse = await fetch(jobUrl, {
-        method: "GET",
-        headers: {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiYWJjZGVmZzAiLCJleHAiOjE3MjA5NzA5MTB9.yg1G0zizw-BoGxcWGUPNgudBhi7CLsM1359eHYMOujQ'
-        }
-    })
-        
-    const responseData = await jobResponse.json()
-    console.log(responseData)
+    $w("#listRepeater").data = []
+    getDataWithGetMethod()
+        .then(data => {
+            console.log("가져온 데이터:", data);
 
-    $w('#listRepeater').data = []
-    initComponents()
-    render()
+            // Repeater에 데이터 연결
+            $w('#listRepeater').data = data.content;
+        initComponents()
+    });
 });
 
 function initComponents() {
     initRepeater()
   }
   
-  async function render(){
-      $w("#listRepeater").data = []
-      var { data, message } = await getDataWithGetMethod({
-      url: "https://asdfdsas.p-e.kr/api/project/list",
-    });
-    console.log(data)
-    $w("#listRepeater").data = []
-    $w("#listRepeater").data = data.content;
-  }
   
   function initRepeater() {
     $w("#listRepeater").onItemReady(($item, itemData, index) => {
       //initItemBackground($item, itemData)
   
       initItemTitle($item, itemData)
+      initItemAddress($item, itemData)
       initItemButtion($item, itemData)
     });
   }
@@ -50,7 +36,11 @@ function initComponents() {
   function initItemTitle($item, itemData) {
     $item("#title").text = itemData.title;
   }
-  
+
+  function initItemAddress($item, itemData) {
+    $item("#text14").text = itemData.address;
+  }
+
   function initItemButtion($item, itemData) {
     $item("#MoreButton").onClick(() => {
       wixLocation.to(`/jobs-4?jobPostId=${itemData.jobPostId}`);
--- src/pages/일자리 생성.bm8v4.js
+++ src/pages/일자리 생성.bm8v4.js
@@ -36,31 +36,31 @@
 //   },
 //   "message": "모집 공고 상세 화면 - 일반 반환 완료"
 // }
+import { getApiKey, kakaoApiKey } from "backend/apikey.jsw";
+
 
 // 페이지가 로드될 때 실행됩니다.
-let i = 0;
-
 $w.onReady(function () {
     $w("#button8").onClick(formSubmit);
 });
 
 // 폼 제출 함수
 async function formSubmit(event) {
-    event.preventDefault(); // 기본 폼 제출 동작 방지
+    //event.preventDefault(); // 기본 폼 제출 동작 방지
 
     // 입력 필드 값 가져오기
-    const jobPostId = i
-    i++;
-    const title = $w('#input1').value;
+    const projectName = $w('#input1').value;
     const tech = $w('#dropdown1').value;
     const people = $w('#input2').value;
     const money = $w('#input3').value;
+    const startDate = $w('#input8').value
+    const endDate = $w('#input8').value
     const workDateResponseList = []
     workDateResponseList.push({ 'workDateId' : 1,
                                 'date' : $w('#input8').value});
     const startTime = $w('#timePicker1').value;
     const endTime = $w('#timePicker2').value;
-    const workAddress = $w('#addressInput1').value;
+    const address = $w('#addressInput1').value;
     const pickup = $w('#radioGroup1').value;
     const pickupAddressList = []
     if (pickup == "True") {
@@ -73,33 +73,42 @@ async function formSubmit(event) {
     const companyName = $w('#input9').value;
     const manager = $w('#input6').value;
     const phone = $w('#input7').value;
-    // 새로운 데이터 객체 생성
+    
+    const latitude = address.location.latitude;
+    const longitude = address.location.longitude;
+ 
 
+    // 새로운 데이터 객체 생성
     const project = {
-        "jobPostId": jobPostId,
-        "title" : title,
-        "tech" : tech,
-        "startTime" : startTime,
-        "endTime" : endTime,
-        "workAddress": workAddress,
-        "distance": null,
-        "meal" : meal,
-        "pickup" : pickup,
-        "pickupAddressList" : pickupAddressList,
-        "park" : park,
-        "parkDetail" : parkDetail,
-        "preparation" : preparation,
-        "workDateResponseList" : workDateResponseList,
-        "companyName" : companyName,
-        "manager" : manager,
-        "phone" : phone,
-        "imageUrls" : []
+        //"jobPostId": jobPostId,
+        "projectName" : projectName,
+        //"tech" : tech,
+        //"startTime" : startTime,
+        //"endTime" : endTime,
+        "startDate" : startDate,
+        "endDate" : endDate,
+        "address" : address.formatted,
+        "latitude" : latitude,
+        "longitude" : longitude
+        //"distance": null,
+        //"meal" : meal,
+        //"pickup" : pickup,
+        //"pickupAddressList" : pickupAddressList,
+        //"park" : park,
+        //"parkDetail" : parkDetail,
+        //"preparation" : preparation,
+        //"workDateResponseList" : workDateResponseList,
+        //"companyName" : companyName,
+        //"manager" : manager,
+        //"phone" : phone,
+        //"imageUrls" : []
     };
     console.log(project)
     const options = {
         method: 'POST',
         headers: {
-            'Content-Type': 'https://asdfdsas.p-e.kr/api/project'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiYWJjZGVmZzAiLCJleHAiOjE3MjA5NzA5MTB9.yg1G0zizw-BoGxcWGUPNgudBhi7CLsM1359eHYMOujQ'
         },
         body: JSON.stringify(project)
     };
@@ -109,7 +118,8 @@ async function formSubmit(event) {
         .then(response => response.json())
         .then(data => {
             // 삽입 성공 시 처리
-            console.log("데이터 삽입 성공:", data);
+            const fullData = { ...project, ...data };
+            console.log("데이터 삽입 성공:", fullData);
             $w('#text142').text = "회원 정보가 성공적으로 등록되었습니다.";
         })
         .catch((error) => {
